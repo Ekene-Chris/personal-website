152984cc7af767ee99032dc277c242ac10601b36
Ekene Chris <ekenechris53@gmail.com>
2025-03-27 09:35:55 +0100

    changed image and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 export const Header = () => {
@@ -28,12 +29,21 @@ export const Header = () => {
     <nav
       className={`fixed w-full z-40 transition-all duration-300 ${
         scrolled
-          ? "bg-black text-linen py-2 shadow-lg"
-          : "bg-black bg-opacity-90 text-linen py-4"
+          ? "bg-white text-black py-2 shadow-lg"
+          : "bg-white text-black py-4"
       }`}
     >
       <div className="container mx-auto px-6 flex justify-between items-center">
         <Link href="/" className="flex items-center">
+          <div className="relative w-10 h-10 mr-3">
+            <Image
+              src="/logo.svg"
+              alt="Ekene Chris Logo"
+              fill
+              className="object-contain"
+              priority
+            />
+          </div>
           <span className="text-xl font-bold">Ekene Chris</span>
         </Link>
 
@@ -41,37 +51,37 @@ export const Header = () => {
         <div className="hidden md:flex space-x-6 items-center">
           <Link
             href="/"
-            className="hover:text-gold transition duration-200 py-2"
+            className="hover:text-caput-mortuum transition duration-200 py-2"
           >
             Home
           </Link>
           <Link
             href="/about"
-            className="hover:text-gold transition duration-200 py-2"
+            className="hover:text-caput-mortuum transition duration-200 py-2"
           >
             About
           </Link>
           <Link
             href="/blog"
-            className="hover:text-gold transition duration-200 py-2"
+            className="hover:text-caput-mortuum transition duration-200 py-2"
           >
             Blog
           </Link>
           <Link
             href="/resources"
-            className="hover:text-gold transition duration-200 py-2"
+            className="hover:text-caput-mortuum transition duration-200 py-2"
           >
             Resources
           </Link>
           <Link
             href="/contact"
-            className="hover:text-gold transition duration-200 py-2"
+            className="hover:text-caput-mortuum transition duration-200 py-2"
           >
             Contact
           </Link>
           <Link
             href="/contact"
-            className="bg-caput-mortuum hover:bg-opacity-90 px-5 py-2 rounded transition duration-200 ml-2"
+            className="bg-caput-mortuum text-white hover:bg-opacity-90 px-5 py-2 rounded transition duration-200 ml-2"
           >
             Work With Me
           </Link>
@@ -81,7 +91,7 @@ export const Header = () => {
         <div className="md:hidden">
           <button
             onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="text-linen focus:outline-none"
+            className="text-black focus:outline-none"
             aria-label="Toggle menu"
           >
             {isMenuOpen ? (
@@ -95,46 +105,46 @@ export const Header = () => {
 
       {/* Mobile Navigation */}
       {isMenuOpen && (
-        <div className="md:hidden bg-black py-4 px-6 absolute w-full shadow-lg">
+        <div className="md:hidden bg-white py-4 px-6 absolute w-full shadow-lg">
           <div className="flex flex-col space-y-4">
             <Link
               href="/"
-              className="block py-2 hover:text-gold transition duration-200"
+              className="block py-2 text-black hover:text-caput-mortuum transition duration-200"
               onClick={() => setIsMenuOpen(false)}
             >
               Home
             </Link>
             <Link
               href="/about"
-              className="block py-2 hover:text-gold transition duration-200"
+              className="block py-2 text-black hover:text-caput-mortuum transition duration-200"
               onClick={() => setIsMenuOpen(false)}
             >
               About
             </Link>
             <Link
               href="/blog"
-              className="block py-2 hover:text-gold transition duration-200"
+              className="block py-2 text-black hover:text-caput-mortuum transition duration-200"
               onClick={() => setIsMenuOpen(false)}
             >
               Blog
             </Link>
             <Link
               href="/resources"
-              className="block py-2 hover:text-gold transition duration-200"
+              className="block py-2 text-black hover:text-caput-mortuum transition duration-200"
               onClick={() => setIsMenuOpen(false)}
             >
               Resources
             </Link>
             <Link
               href="/contact"
-              className="block py-2 hover:text-gold transition duration-200"
+              className="block py-2 text-black hover:text-caput-mortuum transition duration-200"
               onClick={() => setIsMenuOpen(false)}
             >
               Contact
             </Link>
             <Link
               href="/contact"
-              className="bg-caput-mortuum hover:bg-opacity-90 px-4 py-2 rounded inline-block transition duration-200 mt-2"
+              className="bg-caput-mortuum text-white hover:bg-opacity-90 px-4 py-2 rounded inline-block transition duration-200"
               onClick={() => setIsMenuOpen(false)}
             >
               Work With Me
